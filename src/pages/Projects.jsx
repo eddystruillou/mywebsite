f77eaf7270ee5a12a381d8b8d9264feed1fd09de
Eddy Struillou <eddy.s25@hotmail.fr>
2018-05-10 15:56:11 +0200

    Descriptif des projets

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -14,7 +14,7 @@ class Projects extends Component {
   render () {
     return (
       <div>
-        <Jumbotron title="Projets" subtitle="quelque chose ici"/>
+        <Jumbotron title="Projets" subtitle="Quelques-unes de mes réalisations"/>
         <Navbar />
         <div className="container">
           <div className="parallax text-center">
@@ -25,12 +25,11 @@ class Projects extends Component {
             <div className="col-7 pt-5">
               <h2>Intégration Wordpress</h2>
               <p>
-              Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant
-              impression. 
-              Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, 
-              quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un 
-              livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, 
-              mais s'est aussi adapté à la bureautique informatique, sans que 
+              Pour ce projet, le client était une agence immobilière d'une station de montagne.
+              Possédant de nombreux chalets luxueux à vendre elle souhaitait pouvoir mettre à jour
+              sont site régulièrement toute seule.
+              La décision a été de partir sur la création d'un site sous Wordpress en respectant la ligne "luxe"
+              de l'agence.
               </p>
               <a href="http://eddystruillou.com/"><button type="button" className="btn btn-dark">Plus d'info</button></a>
             </div>
@@ -40,14 +39,12 @@ class Projects extends Component {
         <div className="parallax text-center">
           <div className="row">
             <div className="col-7 pt-5">
-              <h2>Solution technique d'une application de restauration</h2>
+              <h2>Documentation et Maquette</h2>
               <p>
-              Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant
-              impression. 
-              Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, 
-              quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un 
-              livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, 
-              mais s'est aussi adapté à la bureautique informatique, sans que 
+              Ici, l'organisatrice du Festival des Films de Plein Air voulait sélectionner et projet des films
+              d'auteur en plein air au pac Monceau à Paris.
+              Le but était de rédiger un document de spécification technique du projet ainsi que présenter une maquette
+              du futur site.
               </p>
               <a href="http://festivaldesfilmsdepleinair.eddystruillou.com/"><button type="button" className="btn btn-dark">Plus d'info</button></a>
             </div>
@@ -70,7 +67,7 @@ class Projects extends Component {
               Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, 
               quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un 
               livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, 
-              mais s'est aussi adapté à la bureautique informatique, sans que 
+              mais s'est aussi adapté à la bureautique informatique, sans que
               </p>
               <a href="http://projet5.eddystruillou.com/"><button type="button" className="btn btn-dark">Plus d'info</button></a>
             </div>
@@ -82,14 +79,12 @@ class Projects extends Component {
             <div className="col-7 pt-5">
               <h2>Jeu de plateau tour par tour</h2>
               <p>
-              Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant
-              impression. 
-              Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, 
-              quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un 
-              livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, 
-              mais s'est aussi adapté à la bureautique informatique, sans que 
+              Conception d'un jeu en ligne en Vanilla JavaScript avec une organisation objet,
+              dans lequel 2 joueurs évoluent chacun leur tour pour s'affronter.
+              J'ai choisi d'utiliser canvas pour la gestion de ma grille pour permettre d'implanter 
+              des animations au cas où le projet vienne à évoluer.
               </p>
-              <a href="http://projet6.eddystruillou.com/"><button type="button" className="btn btn-darko">Plus d'info</button></a>
+              <a href="http://projet6.eddystruillou.com/"><button type="button" className="btn btn-dark">Plus d'info</button></a>
             </div>
             <div className="col-5 pt-5">
               <img src={Projet4} className="img-fluid" alt="jeutourpartour"/>
@@ -105,12 +100,10 @@ class Projects extends Component {
             <div className="col-7 pt-5">
               <h2>Site d'avis de restaurants</h2>
               <p>
-              Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant
-              impression. 
-              Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, 
-              quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un 
-              livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, 
-              mais s'est aussi adapté à la bureautique informatique, sans que 
+              L'objectif ici : créer un service simple et utile qui permet d'avoir des avis 
+              sur des restaurants autour de soi.
+              Pour cela j'ai utilisé les API Google Maps et Google Places ainsi que Géolocalisation.
+              Le projet à lui aussi été réalisé en Vanilla JavaScript avec une organisation objet.
               </p>
               <a href="http://projet7.eddystruillou.com/"><button type="button" className="btn btn-dark">Plus d'info</button></a>
             </div>
@@ -121,13 +114,12 @@ class Projects extends Component {
           <div className="row">
             <div className="col-7 pt-5">
               <h2>Reprendre un projet existant</h2>
-              <p>
-              Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant
-              impression. 
-              Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, 
-              quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un 
-              livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, 
-              mais s'est aussi adapté à la bureautique informatique, sans que 
+              <p> 
+              Dans ce projet de "todo list" que j'ai récupéré, ma misson a été de corrier des bugs, ajouter des tests
+              unitaires avec Jasmine et optimiser les performances.
+              J'ai aussi effectué un audit de performance et une comparaison avec l'audit d'un site concurrent
+              en rédigeant un wiki sur mon profil github.
+              Pour finir, j'ai écrit un "guide utilisateur" ainsi qu'une documentation technique du projet et de son fonctionnement.
               </p>
               <a href="http://projet8.eddystruillou.com/"><button type="button" className="btn btn-dark">Plus d'info</button></a>
             </div>
